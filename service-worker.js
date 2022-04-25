fe490e60f0731734753730e2a80388178840c54e
Norman <>
2022-04-25 17:54:56 +0200

    init demo

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static")
+            .then(cache => {
+                return cache.addAll([
+                    "./",
+                    "./src/style.css",
+                    "./assets/img/logo192.png"
+                ])
+            })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(res => {
+                return res || fetch(e.request);
+            })
+    );
+});
